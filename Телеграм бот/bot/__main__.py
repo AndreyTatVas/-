ff08f7b6e48f4f30288c604388d72ac98a785585
Andrey Tatarenko <a.tatarenko@cism-mc.ru>
2023-07-19 01:28:29 +0300

    Добавление части урока по созданию команд

--- Телеграм бот/bot/__main__.py
+++ Телеграм бот/bot/__main__.py
@@ -3,14 +3,20 @@ import asyncio
 import logging
 
 from aiogram import Dispatcher, Bot
+from aiogram.types import BotCommand
 
 from bot.commands import register_user_commands
+from bot.commands.bot_commands import bot_commands
 
 
 async def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
+
+    commands_for_bot = [BotCommand(command=cmd[0], description=cmd[1]) for cmd in bot_commands]
+
     db = Dispatcher()
     bot = Bot(token=os.getenv('token'))
+    await bot.set_my_commands(commands=commands_for_bot)
 
     register_user_commands(db)
 
--- Телеграм бот/bot/commands/__init__.py
+++ Телеграм бот/bot/commands/__init__.py
@@ -1,10 +1,12 @@
-__all__ = ['register_user_commands']
+__all__ = ['register_user_commands', 'bot_commands']
 
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
+from bot.commands.help import help_command
 from bot.commands.start import start
 
 
 def register_user_commands(router: Router) -> None:
     router.message.register(start, CommandStart())
+    router.message.register(help_command, Command(commands=['help']))
